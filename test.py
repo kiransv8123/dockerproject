173ccec3dd529a7a6f75a7e0a738efa77060bf7b
kiransv <kiransv8123@gmail.com>
2023-07-27 19:59:51 +0530

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def test_get_user():
+    response = requests.get("https://reqres.in/api/users?page=2")
+    assert response.status_code == 200
+
+
+def test_create_user():
+    data = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }
+    response = requests.post("https://reqres.in/api/register", json=data)
+    assert response.status_code == 200
+    assert response.json()["id"] == 4
